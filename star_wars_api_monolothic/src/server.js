b1d13ef467483b8882f954e060068b9fb01af82a
FelipeJure <felipe.jure05@gmail.com>
2023-07-15 18:47:05 +0200

    create two directories

--- star_wars_api_monolothic/src/server.js
+++ star_wars_api_monolothic/src/server.js
@@ -6,6 +6,6 @@ const server = express();
 server.use(express.json());
 server.use(morgan("dev"));
 
-server.use(require("./routes"));
+server.use(require("../src/routes"));
 
 module.exports = server;
