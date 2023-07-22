acbec78a858c3b92b4ed0f0fdf185625b55150a8
FelipeJure <felipe.jure05@gmail.com>
2023-07-22 15:43:55 +0200

    create validation of Body

--- star_wars_api_microservices/database/src/middlewares/validateBody.js
+++ star_wars_api_microservices/database/src/middlewares/validateBody.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
-    console.log(req.body)
     if(!req.body.hasOwnProperty('_id')){
-        next('Debe crear con un ID valido')
+        next('Debe crear con un ID')
     }
 }
