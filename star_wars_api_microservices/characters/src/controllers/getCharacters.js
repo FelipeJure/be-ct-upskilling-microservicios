4448cf9a110c46a49c4b4eb412dfa5ca4e82bc33
FelipeJure <felipe.jure05@gmail.com>
2023-07-22 19:22:38 +0200

    remove star_wars monolitic

--- star_wars_api_microservices/characters/src/controllers/getCharacters.js
+++ star_wars_api_microservices/characters/src/controllers/getCharacters.js
@@ -1,7 +1,9 @@
-const Characters = require('../data')
-const { response } = require('../utils')
+const Characters = require('../data');
+const { response } = require('../utils');
+const axios = require('axios');
 
 module.exports = async (req, res) => {
+    axios.get("http://database:8004/Character")
     const characters = await Characters.list()
     response(res, 200, characters)
 }
--- star_wars_api_monolothic/README.md
+++
@@ -1,3 +0,0 @@
-# Curso Upskilling
-
-En este curso se implementa la utilidad de microservicios con Docker
